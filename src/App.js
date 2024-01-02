25bbda9864e3ca04bc9e2d394d8d31db7fe53c2e
mdev-new <zdenda.hrouda@outlook.cz>
2024-01-02 21:47:38 +0100

    work

--- src/App.js
+++ src/App.js
@@ -2,25 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Helpdesk from './pages/helpdesk'
+
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from "react-router-dom";
+
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+firebase.initializeApp({
+	apiKey: "AIzaSyDTmT_TH9iBdX4BRsIvBUTndbP5NG9OD24",
+	authDomain: "delta-homepage.firebaseapp.com",
+	databaseURL: "https://delta-homepage-default-rtdb.firebaseio.com",
+	projectId: "delta-homepage",
+	storageBucket: "delta-homepage.appspot.com",
+	messagingSenderId: "1073883844055",
+	appId: "1:1073883844055:web:2399adeca960b4a57d0f92"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
+const Auth = () => {
+
+	const signInWithGgl = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		auth.signInWithPopup(provider);
+	}
+
+	return !auth.currentUser ? (
+		<button onClick={signInWithGgl}>Prihlasit se s Googlem</button>
+	) : (
+		<button onClick={() => auth.signOut()}>Sign out</button>
+	);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [user] = useAuthState(auth);
+
+	return (
+		<>
+		<Auth />
+		<Router>
+		<Routes>
+			<Route exact path="/helpdesk" element={<Helpdesk user={auth.currentUser} firestore={firestore} />} />
+		</Routes>
+		</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/helpdesk/index.jsx
@@ -0,0 +1,109 @@
+import {
+	Button,
+	Input,
+	Stack,
+	Table,
+	Typography,
+	FormControl,
+	FormLabel,
+	Select,
+	MenuItem,
+	Option
+} from '@mui/joy'
+
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import firebase from 'firebase/compat/app'
+
+export default function Helpdesk({user, firestore}) {
+
+	const problemsCol = firestore.collection('problems')
+	const query = problemsCol.orderBy('createdAt', 'desc')
+
+	const [problems] = useCollectionData(query, {idField: 'id'})
+
+	const submit = (event) => {
+		event.preventDefault();
+
+    const form = event.target;
+    const formFields = form.elements;
+
+		const date = new Date();
+		problemsCol.add({
+			problem: formFields.problem.value,
+			type: formFields.type[1].value,
+			place: formFields.place.value,
+			assigned: formFields.assignee.value,
+			reporter: user.displayName,
+			datetime: `${date.getDate()}.${date.getMonth()+1}. ${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, 0)}`,
+			state: 'waiting',
+			createdAt: firebase.firestore.FieldValue.serverTimestamp()
+		})
+	}
+
+	return (<>
+					{ user &&
+           <form onSubmit={submit}>
+             <Stack direction="row">
+                 <FormControl>
+                     <FormLabel>
+                         Závada
+                     </FormLabel>
+                     <Input name="problem" variant="outlined" />
+                 </FormControl>
+                 <FormControl>
+                     <FormLabel>
+                         Místo
+                     </FormLabel>
+                     <Input name="place" variant="outlined" />
+                 </FormControl>
+                 <FormControl>
+                     <FormLabel>
+                         Přiřazená osoba (e-mail)
+                     </FormLabel>
+                     <Input name="assignee" variant="outlined" />
+                 </FormControl>
+                 <FormControl sx={{width: 120}}>
+                     <FormLabel>Typ závady</FormLabel>
+                     <Select
+                         label="Typ závady"
+                         name="type"
+                     >
+                         <Option value="Hardware">Hardware</Option>
+                         <Option value="Software">Software</Option>
+                         <Option value="Ostatni">Ostatní</Option>
+                     </Select>
+                 </FormControl>
+                 <Button type="submit" variant="contained">Přidat</Button>
+             </Stack>
+           </form>
+					}
+
+       		 <Table>
+             <thead>
+                <tr>
+                    <th><b>Závada</b></th>
+                    <th><b>Typ závady</b></th>
+                    <th><b>Místo</b></th>
+                    <th><b>Datum a čas</b></th>
+                    <th><b>Přiřazená osoba</b></th>
+                    <th><b>Nahlásil</b></th>
+                    <th><b>Souhlasí</b></th>
+                </tr>
+             </thead>
+             <tbody>
+                 {problems && problems.map((post) => 
+                     <tr style={(post.state == 'waiting') ? {} : (post.state == 'work') ? {color: 'yellow'} : {}}>
+                         <td><Typography>{post.problem}</Typography></td>
+                         <td><Typography>{post.type}</Typography></td>
+                         <td><Typography>{post.place}</Typography></td>
+                         <td><Typography>{post.datetime}</Typography></td>
+                         <td><Typography>{post.assigned}</Typography></td>
+                         <td><Typography>{post.reporter}</Typography></td>
+                         <td><Typography>{post.liked_by}</Typography></td>
+                     </tr>
+                 )}
+             </tbody>
+            </Table>
+    </>);
+
+}
